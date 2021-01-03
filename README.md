ab61c3dff05da456342a8c3b0a060b6c92ca233f
butlerx <butlerx@notthe.cloud>
2021-01-03 21:52:46 +0000

    close issue #1

---
+++ README.md
@@ -0,0 +1,12 @@
+# channels
+
+Channels is a distributed multi-channel websocket based streaming data service.
+Channels Server allows for publishing arbitrary data that can be consumed by
+multiple consumers.
+
+Channels are low cost to create and delete allowing for user to send generate
+shared secrets to send and consume data from without needing to configure
+servers.
+
+Channels is designed for easy webhook integration, etl and buffering of
+distributed workloads.
---
+++ examples/etl.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// Requires `npm i websocket fetch` to work
+
+const WebSocket = require('websocket').w3cwebsocket;
+const fetch = require('node-fetch');
+
+const listen = 'unstructured';
+
+const ws = new WebSocket(`ws://localhost:3030/api/v1/events/${listen}`);
+
+ws.addEventListener('message', (event) => {
+  fetch('http://localhost:3030/api/v1/events/structured', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ msg: event.data, etl: true, timestamp: Date.now() }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+});
